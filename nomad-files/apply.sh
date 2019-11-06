e8695a138f2ae2a04286681f0e783acb6acf171f
Matthieu Jacquot <matthieu.jacquot.tech@protonmail.ch>
2019-11-06 21:53:25 +0100

    improve nomad apply.sh

--- nomad-files/apply.sh
+++ nomad-files/apply.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+job_name_from_file='\.\/(.*)\.hcl'
+
 if [ $1 == "all" ]
 then
-    echo "applying all jobs"
-    for job in statsd jaeger loki prometheus ingress echo1 echo2 chaos promtail
+    echo "applying all"
+    for file in ./*
     do
-        nomad job stop -purge "$job"
-        nomad run "$job".hcl
-     done
+        if [[ -f $file && $file =~ $job_name_from_file ]]; then
+            job="${BASH_REMATCH[1]}"
+            nomad job stop -purge "$job"
+            nomad run "$job".hcl
+        fi
+    done
 else
     echo "applying $1"
-    nomad job stop -purge "$1"
-    nomad run "$1".hcl
+
+    if [[ $1 =~ $job_name_from_file ]]; then
+        job="${BASH_REMATCH[1]}"
+        nomad job stop -purge "$job"
+        nomad run "$job".hcl
+    fi
 fi
